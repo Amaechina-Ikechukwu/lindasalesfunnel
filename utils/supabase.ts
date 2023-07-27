4dd3567a5a0794f7c3f8d52265aaa2fa92b019ef
unknown <amaechinaikechukwu6@gmail.com>
2023-07-27 22:19:33 +0100

    fixed env

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,5 @@
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 export default createClient(
-process.env.SUPABASE_URL!,
-process.env.SUPABASE_ANON_KEY!
-)
+  "https://infotwzujoiethkrmalt.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZm90d3p1am9pZXRoa3JtYWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwMjM0OTYsImV4cCI6MjAwNDU5OTQ5Nn0.5iDeBtPO8r7DqdalCuipDIUVUULEL81vxnW2iyieo0g"
+);
