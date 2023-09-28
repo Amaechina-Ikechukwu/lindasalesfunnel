26ed518ceaf3c18fa79602f772916ddfd9bc0ece
unknown <amaechinaikechukwu6@gmail.com>
2023-09-28 17:01:31 +0100

    added permissions for instagram

--- app/instagram/confirm/page.tsx
+++ app/instagram/confirm/page.tsx
@@ -1,44 +1,64 @@
-'use client'
- 
-import { useSearchParams } from 'next/navigation'
+"use client";
 
-
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 export default function Page() {
   const searchParams = useSearchParams(); // Use array destructuring to get searchParams
-  const search = searchParams.get('code');
-
+  const search = searchParams.get("code");
+  const router = useRouter();
   async function getUserToken() {
-    const redirectUri = 'https://aa8b-129-205-113-160.ngrok-free.app/instagram/confirm';
-
     try {
-      const res = await fetch('http://localhost:3003/instagram/confirm?code='+search, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+      const res = await fetch(
+        "http://localhost:3003/instagram/confirm?code=" + search,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
         }
-      });
+      );
 
       if (!res.ok) {
-        throw new Error('Failed to fetch data');
+        throw new Error("Failed to fetch data");
       }
 
-      const data = await res.json();
-      console.log(data);
-
+      const { uid } = await res.json();
+      console.log(uid);
+      handleLocalStorageUpdate(uid);
       // Handle the Instagram response data as needed
-    } catch (error) {
-      console.error('Error:', error.message);
+    } catch (error: any) {
+      console.error("Error:", error.message);
     }
   }
+  const handleLocalStorageUpdate = (uid: string) => {
+    // Update local storage when needed
+    if (uid !== undefined) {
+      localStorage.setItem("IGtoken", uid);
+    }
+    router.push("/instagram");
+
+    return null;
+  };
 
   return (
     <>
-      <button
-        onClick={getUserToken}
-        className="bg-blue-500 w-full text-white rounded-md hover:text-white shadow-md p-4 drop-shadow-xl hover:scale-105 active:scale-95 scale-100 hover:transition ease-in-out delay-50 text-center"
-      >
-        Complete Connection
-      </button>
+      <div className="container mx-auto px-6 py-12 h-screen">
+        <div className="space-y-8 flex flex-col  h-full items-center justify-center">
+          <div className="space-y-8 flex flex-col items-center  sm:w-3/5 md:w-4/5">
+            <div className="space-y-4 sm:space-y-8">
+              <p className="text-xl font-xl">
+                Please, complete the connection process
+              </p>
+              <button
+                onClick={() => getUserToken()}
+                className="bg-blue-500 w-full text-white rounded-md hover:text-white shadow-md p-4 drop-shadow-xl hover:scale-105 active:scale-95 scale-100 hover:transition ease-in-out delay-50 text-center"
+              >
+                Complete Connection
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- app/instagram/page.tsx
+++ app/instagram/page.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
+import * as React from "react";
 import Image from "next/image";
 import LindaImage from "@/assets/Linda.png";
-import AIImage from "@/assets/ai.jpg";
+import Button from "@mui/joy/Button";
+import Modal from "@mui/joy/Modal";
+import ModalClose from "@mui/joy/ModalClose";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
 import FBImage from "@/assets/facebook.jpg";
-import Button from "@/style/Button";
-import Link from "next/link";
-
-export default async function Pages() {
+import { useEffect, useState } from "react";
+export default function Pages() {
   //   const handleInstagramAuth = () => {
   //   const clientId = '6600635150017636'; // Replace with your Instagram App's client ID
-  //   const redirectUri = 'https://aa8b-129-205-113-160.ngrok-free.app/instagram/confirm'; // Replace with your redirect URI
+  //   const redirectUri = 'https://ebc5-129-205-113-159.ngrok-free.app/instagram/confirm'; // Replace with your redirect URI
   //   const scope = 'user_profile';
   //   const responseType = 'code';
 
@@ -18,57 +21,85 @@ export default async function Pages() {
   //   // Open a new window with the Instagram authorization URL
   //   window.open(authUrl, 'InstagramAuthWindow', 'width=500,height=600');
   // };
+  const [dataFromLocalStorage, setDataFromLocalStorage] = useState("");
+
+  useEffect(() => {
+    // Access local storage when the component mounts
+    const localStorageData = localStorage.getItem("usertoken");
+    if (localStorageData) {
+      setDataFromLocalStorage(localStorageData);
+    } else {
+      setOpen(true);
+    }
+    console.log(localStorageData?.length);
+  }, []);
+
   const handleInstagramAuth = () => {
-  const clientId = '6600635150017636'; // Replace with your Instagram App's client ID
-  const redirectUri = 'https://aa8b-129-205-113-160.ngrok-free.app/instagram/confirm'; // Replace with your redirect URI
-  const scope = 'user_profile';
-  const responseType = 'code';
+    const clientId = "6600635150017636"; // Replace with your Instagram App's client ID
+    const redirectUri =
+      "https://ebc5-129-205-113-159.ngrok-free.app/instagram/confirm"; // Replace with your redirect URI
+    const scope = "user_profile";
+    const responseType = "code";
 
-  const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+    const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
 
-  // Redirect the current tab to the Instagram authorization URL
-  window.location.href = authUrl;
-};
+    // Redirect the current tab to the Instagram authorization URL
+    window.location.href = authUrl;
+  };
+  const [open, setOpen] = React.useState<boolean>(false);
+  if (dataFromLocalStorage.length) {
+    return (
+      <div className="container mx-auto px-6 py-12 h-screen">
+        <div className="space-y-8 flex flex-col sm:items-center">
+          <div className="space-y-8 flex flex-col items-center  sm:w-3/5 md:w-4/5">
+            <div className="space-y-4 sm:space-y-8">
+              <h1 className="text-gray-600 text-center">
+                Linda Meets Instagram
+              </h1>
+              <h3 className="text-gray-500 text-center">
+                Do not allow the distrations of Instagram waste your time. Sieve
+                through responses from your ads, answer clients, and much more
+              </h3>
+            </div>
+            <div className="w-full flex justify-center">
+              <button
+                onClick={() => handleInstagramAuth()}
+                className="bg-blue-500 w-full text-white rounded-md hover:text-white shadow-md p-4 drop-shadow-xl hover:scale-105 active:scale-95 scale-100 hover:transition ease-in-out delay-50 text-center"
+              >
+                Connect To Linda
+              </button>
+            </div>
+          </div>
 
-
+          <div className="flex flex-col sm:flex-row items-center sm:justify-center space-y-4 space-x-4 sm:w-3/5">
+            <Image
+              src={LindaImage}
+              width={400}
+              height={400}
+              className="drop-shadow-xl hover:scale-105 w-60"
+              alt="Linda"
+            />
+            <h1 className="font-bold">+</h1>
+            <Image
+              src={FBImage}
+              width={400}
+              height={400}
+              className="drop-shadow-xl hover:scale-105 w-60"
+              alt="Facebook"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container mx-auto px-6 py-12 h-screen">
       <div className="space-y-8 flex flex-col sm:items-center">
         <div className="space-y-8 flex flex-col items-center  sm:w-3/5 md:w-4/5">
           <div className="space-y-4 sm:space-y-8">
-            <h1 className="text-gray-600 text-center">
-              Linda Meets Instagram
-            </h1>
-            <h3 className="text-gray-500 text-center">
-              Do not allow the distrations of Instagram waste your time. Sieve through responses from your ads, answer clients, and much more
-            </h3>
+            <h1 className="text-gray-600 text-center">Linda Meets Instagram</h1>
+            <h3 className="text-gray-500 text-center">Welcome</h3>
           </div>
-          <div className="w-full flex justify-center">
-            <button onClick={()=>handleInstagramAuth()}
-              className="bg-blue-500 w-full text-white rounded-md hover:text-white shadow-md p-4 drop-shadow-xl hover:scale-105 active:scale-95 scale-100 hover:transition ease-in-out delay-50 text-center"
-            >
-              Connect To Linda
-            </button>
-          </div>
-        </div>
-
-        <div className="flex flex-col sm:flex-row items-center sm:justify-center space-y-4 space-x-4 sm:w-3/5">
-          <Image
-            src={LindaImage}
-            width={400}
-            height={400}
-            className="drop-shadow-xl hover:scale-105 w-60"
-            alt="Linda"
-          />
-          <h1 className="font-bold">+</h1>
-          <Image
-            src={FBImage}
-            width={400}
-            height={400}
-            className="drop-shadow-xl hover:scale-105 w-60"
-            alt="Facebook"
-          />
-        
         </div>
       </div>
     </div>
